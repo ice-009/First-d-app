23c4fd9130e6005aeafc35a4ec767e93e2d2dda0
ice-009 <21bma009@nith.ac.in>
2023-05-22 12:37:28 +0530

    dapp ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import Buy from './components/buy';
+import List from './components/List'
+import { ethers } from 'ethers';
+import abi from "./contract/chai.json";
 import './App.css';
+// import List from './components/List';
 
 function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  const [account, setAccount] = useState("None");
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0x0c2d455aB62Be3856A5D2C48B5F98DE7511AE2d6";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+
+          window.ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+
+          window.ethereum.on("accountsChanged", () => {
+            window.location.reload();
+          });
+
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+          );
+          setAccount(account);
+          setState({ provider, signer, contract });
+        } else {
+          alert("Please install metamask");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  }, []);
+  // console.log(state);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{ backgroundColor: "#EFEFEF", height: "100%" }}>
+    <p
+        class="text-muted lead "
+        style={{ marginTop: "10px", marginLeft: "5px" }}
+      >
+        <small>Connected Account - {account}</small>
+      </p>
+      <div className="container">
+        <Buy state={state} />
+      <List state= {state}></List>
     </div>
+    </div>
+    </>
   );
 }
 
